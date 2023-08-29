d43b2378ca35c924562bdfcd56dcc9c9f545e97b
taiki yamamoto <ultra0115.20@gmail.com>
2023-08-29 15:53:00 +0900

    titleカラムにNOT NULL制約追加、newとeditのフォームにrequired追加

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2023_08_29_023105) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
