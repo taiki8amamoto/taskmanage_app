f0f0dfc2322b5600b1d72a92510241dc3a0efec5
taiki yamamoto <ultra0115.20@gmail.com>
2023-08-29 15:29:10 +0900

    new, create, edit, update機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,41 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "Taskを登録しました！"
+    else
+      render :new
+    end
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.update
+      redirect_to tasks_path, notice: "Taskを更新しました！"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(tasks).permit(:title, :content)
   end
 end
